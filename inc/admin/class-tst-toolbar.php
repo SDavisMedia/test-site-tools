322a7aabfdd9e3e3b45a2436b178fd38cee77bd2
Sean Davis <sdavis2702@gmail.com>
2014-03-01 15:09:23 -0600

    add ability to delete current post from toolbar

--- inc/admin/class-tst-toolbar.php
+++ inc/admin/class-tst-toolbar.php
@@ -26,10 +26,10 @@ class TST_Toolbar {
 	 * @since 1.0.0
 	 */
 	public function tst_toolbar( $wp_admin_bar ) {
+	
 
 		/**
-		 * There's no need to show the link from the WordPress dashboard. Also, don't 
-		 * show it if the user doesn't have permission to access the Plugins screen.
+		 * only show link if on the front-end and user can activate plugins
 		 */
 		if ( is_admin() || ! current_user_can( 'activate_plugins' ) ) {
 			return;
@@ -43,9 +43,9 @@ class TST_Toolbar {
 			) );
 		}
 		
+		
 		/**
-		 * There's no need to show the link from the WordPress dashboard. Also, don't 
-		 * show it if the user doesn't have permission to access the Tools screen.
+		 * only show links if on the front-end and user can acces Tools menu
 		 */
 		if ( is_admin() || ! current_user_can( 'import' ) ) {
 			return;
@@ -64,6 +64,21 @@ class TST_Toolbar {
 					'id'     => 'regen-thumbs',
 					'title'  => __( 'Regenerate Thumbs', 'tst' ),
 					'href'   => admin_url( 'tools.php?page=regenerate-thumbnails' ) 
+				) );
+			}
+			
+	
+			/**
+			 * show "Move to Trash" link if user has the capability and is on post/page
+			 */
+			$current_object = get_queried_object();
+			if ( ! is_super_admin() || ! is_admin_bar_showing() || empty( $current_object ) ) {
+				return;
+			} elseif ( ! empty( $current_object->post_type ) && ( $post_type_object = get_post_type_object( $current_object->post_type ) ) && current_user_can( $post_type_object->cap->edit_post, $current_object->ID ) ) { 
+				$wp_admin_bar->add_menu( array( 
+					'id'	=> 'delete',
+					'title'	=> __( 'Move to Trash' ),
+					'href'	=> get_delete_post_link( $current_object->term_id )
 				) );
 			}
 		}
